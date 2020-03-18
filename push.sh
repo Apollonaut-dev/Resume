f90f2fa0dad7b645a49ec095926905d2f7354b60
Anthony <anthonydevellis@gmail.com>
2020-03-18 17:07:54 -0400

    update

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 pdflatex main.tex
-convert -verbose -flatten -density 500 -resize '1080' main.pdf main.png
+convert -verbose -flatten -density 500 -resize '1080' main.pdf main.jp2
 git add .
 git commit -m "update"
 git push origin master
