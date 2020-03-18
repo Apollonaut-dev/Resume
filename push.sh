a6080193a3b1432f5b25c0dec6d1bb72495ee80c
Anthony <anthonydevellis@gmail.com>
2020-03-18 18:47:42 -0400

    update

--- push.sh
+++ push.sh
@@ -1,5 +1,6 @@
 pdflatex main.tex
 convert -verbose -flatten -density 500 -resize '1080' main.pdf main.jpg
+cwebp -q 50 main.jpg -o main.webp
 git add .
 git commit -m "update"
 git push origin master
