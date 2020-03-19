28247a7042af8a3bf462b699124673d3c2b6e5b5
Anthony <anthonydevellis@gmail.com>
2020-03-19 19:09:03 -0400

    update

--- push.sh
+++ push.sh
@@ -1,6 +1,13 @@
+# Prepare 1 page resume output
 pdflatex main.tex
 convert -verbose -flatten -density 500 -resize '1080' main.pdf main.jpg
 cwebp -q 50 main.jpg -o main.webp
+
+# Prepare full resume output
+pdflatex main_full.tex 
+convert -verbose -flatten -density 500 -resize '1080' main_full.pdf main_full.jpg
+cwebp -q 50 main_full.jpg -o main.webp
+
 git add .
 git commit -m "update"
 git push origin master
