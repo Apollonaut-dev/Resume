4bb33f4bbcb72fc13c8fc70bc1f7511caa796145
Anthony <anthonydevellis@gmail.com>
2020-03-19 19:09:17 -0400

    update

--- push.sh
+++ push.sh
@@ -4,9 +4,9 @@ convert -verbose -flatten -density 500 -resize '1080' main.pdf main.jpg
 cwebp -q 50 main.jpg -o main.webp
 
 # Prepare full resume output
-pdflatex main_full.tex 
-convert -verbose -flatten -density 500 -resize '1080' main_full.pdf main_full.jpg
-cwebp -q 50 main_full.jpg -o main.webp
+# pdflatex main_full.tex 
+# convert -verbose -flatten -density 500 -resize '1080' main_full.pdf main_full.jpg
+# cwebp -q 50 main_full.jpg -o main.webp
 
 git add .
 git commit -m "update"
